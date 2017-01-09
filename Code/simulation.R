5bdfd4b038b055241d31a383e38a05e2a81326ab
Lucy Li <mengqi.li09@imperial.ac.uk>
2017-01-09 15:46:43 -0500

    simulate data

--- Code/simulation.R
+++ Code/simulation.R
@@ -1,18 +1,26 @@
 source("functions.R")
 
-sim.data <- simulate.data(
-  N=1000, ntypes=13, ntot=20, nswabs=5, times=c(365/12, 6*365/12, 7*365/12, 365, 18*365/12),
-  lambda=c(0.005,0.0037,0.002,0.0025,0.003,0.0019,0.0015,0.002,0.002,0.0027,0.0018,0.0019,0.002, # vaccine types
-           0.003689, 0.002932, 0.003226, 0.002592, 0.003269, 0.002542, 0.003142)*10, # nonvaccine types
-  mu=c(0.0333,0.0333,0.0286,0.0286,0.0286,0.0286,0.0222,0.0333,0.0333,0.0333,0.025,0.0143,0.0286, #vaccine types
-       0.02914, 0.02736, 0.0272, 0.03303, 0.03161, 0.03369, 0.03009), #nonvaccine types
-  p0=c(0.46, 0.3, 0.41, 0.16, 0.175, 0.287, 0.435, 0.178, 0.368, 0.217, 0.389, 0.372, 0.219)*2,
-  thetaSI=c(0.632, 0.476, 0.518, 0.617, 0.496, 0.53, 0.54, 0.682, 0.602, 0.639, 0.555, 0.618, 0.507),
-  thetaIS=1/c(0.854, 1, 0.723, 0.919, 0.859, 1, 1, 0.891, 1, 0.911, 1, 0.918, 0.84),
-  frailtySI=0.2, frailtyIS=0.8, interaction=0.4)
+sim.params <- list(N=1000, ntypes=13, ntot=20, nswabs=5, times=c(365/12, 6*365/12, 7*365/12, 365, 18*365/12),
+     lambda=rnorm(20, 0.0026696, 7.239943e-07),#c(0.005,0.0037,0.002,0.0025,0.003,0.0019,0.0015,0.002,0.002,0.0027,0.0018,0.0019,0.002, # vaccine types
+            #  0.003689, 0.002932, 0.003226, 0.002592, 0.003269, 0.002542, 0.003142), # nonvaccine types
+     mu=rnorm(20, 0.0026696, 7.239943e-07),#c(0.0333,0.0333,0.0286,0.0286,0.0286,0.0286,0.0222,0.0333,0.0333,0.0333,0.025,0.0143,0.0286, #vaccine types
+        #  0.02914, 0.02736, 0.0272, 0.03303, 0.03161, 0.03369, 0.03009)/10, #nonvaccine types
+     p0=c(0.16, 0.3, 0.141, 0.16, 0.175, 0.287, 0.135, 0.178, 0.308, 0.217, 0.319, 0.322, 0.219)*2.5,
+     thetaSI=c(0.632, 0.476, 0.518, 0.617, 0.496, 0.53, 0.54, 0.682, 0.602, 0.639, 0.555, 0.618, 0.507),
+     thetaIS=1/c(0.854, 1, 0.723, 0.919, 0.859, 1, 1, 0.891, 1, 0.911, 1, 0.918, 0.84),
+     frailtySI=0.2, frailtyIS=0.2, interaction=0.4,
+     ab.noise=runif(ntypes, 0.75))
 
-# Compare the number of samples testing positive for vaccine types
-Reduce('+', lapply(sim.data[, -1:-2], function (x) c(sum(x[1:(N/2)]<=(ntypes+1)), sum(x[(N/2+1):N]<=(ntypes+1)))))
+theta.SI.agr <- with(sim.params, get_agr_SI(lambda[1:ntypes], thetaSI))
+theta.IS.agr <- with(sim.params, get_agr_IS(lambda, mu, thetaSI, thetaIS, ntypes))
 
+variability <- sapply(20170109:20170139, function (x) {
+  set.seed(x)
+  sim.data <- do.call(simulate_data, sim.params)
+  sim.data.summ <- summarize_data(sim.data, sim.params$ntypes)
+  colSums(sim.data.summ$mean.num.samples)
+})
+apply(variability, 1, sd)
 
+sim.data <- do.call(simulate_data, sim.params)
 
