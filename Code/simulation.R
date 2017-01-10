fb98961711a2cc176c414d082718bcc065409101
Lucy Li <mengqi.li09@imperial.ac.uk>
2017-01-10 09:14:32 -0500

    simulate data

--- Code/simulation.R
+++ Code/simulation.R
@@ -9,18 +9,19 @@ sim.params <- list(N=1000, ntypes=13, ntot=20, nswabs=5, times=c(365/12, 6*365/1
      thetaSI=c(0.632, 0.476, 0.518, 0.617, 0.496, 0.53, 0.54, 0.682, 0.602, 0.639, 0.555, 0.618, 0.507),
      thetaIS=1/c(0.854, 1, 0.723, 0.919, 0.859, 1, 1, 0.891, 1, 0.911, 1, 0.918, 0.84),
      frailtySI=0.2, frailtyIS=0.2, interaction=0.4,
-     ab.noise=runif(ntypes, 0.75))
+     ab.noise=runif(ntypes, 0.2, 0.6))
 
 theta.SI.agr <- with(sim.params, get_agr_SI(lambda[1:ntypes], thetaSI))
 theta.IS.agr <- with(sim.params, get_agr_IS(lambda, mu, thetaSI, thetaIS, ntypes))
 
-variability <- sapply(20170109:20170139, function (x) {
-  set.seed(x)
-  sim.data <- do.call(simulate_data, sim.params)
-  sim.data.summ <- summarize_data(sim.data, sim.params$ntypes)
-  colSums(sim.data.summ$mean.num.samples)
-})
-apply(variability, 1, sd)
-
+# variability <- sapply(20170109:20170139, function (x) {
+#   set.seed(x)
+#   sim.data <- do.call(simulate_data, sim.params)
+#   sim.data.summ <- summarize_data(sim.data, sim.params$ntypes)
+#   colSums(sim.data.summ$mean.num.samples)
+# })
+# apply(variability, 1, sd)
+set.seed(1000)
 sim.data <- do.call(simulate_data, sim.params)
-
+#save(sim.data, file="sim.data.RData")
+cor(sim.data[sim.data$vacc, -1:-7])
