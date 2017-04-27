fcc419f4505e80e92f9c26623c0185ee765c1e10
Lucy Li <mengqi.li09@imperial.ac.uk>
2017-04-27 14:01:22 -0400

    update simulation code

--- Code/simulation.R
+++ Code/simulation.R
@@ -1,26 +1,27 @@
 #setwd("~/Dropbox (hsph.harvard.edu)/Research/Projects/Vaccine/VaccEffCorr/Code")
 source("functions.R")
+library(parallel)
 ntypes = 13
-ntot = 31
+ntot = 33
 day.units = 1
-nswabs = 6
-sim.params <- list(Nv=200, Nnv=200, ntypes=ntypes, ntot=ntot, nswabs=nswabs,
-                   times=(c(2, 7, 12, 13, 18, 24)[1:nswabs])*30/day.units,
+nswabs = 3
+sim.params <- list(Nv=700, Nnv=0, ntypes=ntypes, ntot=ntot, nswabs=nswabs,
+                   times=(c(6, 7, 12)[1:nswabs])*30/day.units,
      lambda=c(rlnorm(ntypes, log(0.0003696), 2e-1), rlnorm(ntot-ntypes, log(0.0003696), 2e-1))*day.units,
      mu=rlnorm(ntot, log(0.016696), 2e-1)*day.units,
      thetaSI=rnorm(ntypes, 0.2505, 0.06),
      thetaIS=rep(1, ntypes),#1/rnorm(ntypes, 1, 0.06)[1:ntypes],
-     p0=rep(0, ntypes),#abs(rnorm(ntypes, 0.5, 0.05)),
-     frailtySI=0.2, frailtyIS=0.2, interaction=0.99)
+     p0=rnorm(ntypes, 0.9, 0.3),#abs(rnorm(ntypes, 0.5, 0.05)),
+     frailtySI=0.2, frailtyIS=0.2, interaction=0.4,
+     ab.mean=0.3, ab.var=0.45)
 sim.params.vec <- unlist(sim.params[-1:-5])
 sim.params.sd <- list(lambda=rep(mean(sim.params$lambda)*0.01, ntot), 
                       mu=rep(mean(sim.params$mu)*0.01, ntot), 
                       #thetaSI=0.3, thetaIS=0.1, p0=0.2, frailtySI=0.2, frailtyIS=0.2, 
                       interaction=0.2)
-sim.params$ab.noise <- runif(sim.params$ntypes, 0.0, 0.05)
 
-theta.SI.agr <- with(sim.params, get_agr_SI(lambda[1:ntypes], thetaSI))
-theta.IS.agr <- with(sim.params, get_agr_IS(lambda, mu, thetaSI, thetaIS, ntypes))
+# theta.SI.agr <- with(sim.params, get_agr_SI(lambda[1:ntypes], thetaSI))
+# theta.IS.agr <- with(sim.params, get_agr_IS(lambda, mu, thetaSI, thetaIS, ntypes))
 
 # variability <- sapply(20170109:20170139, function (x) {
 #   set.seed(x)
@@ -29,16 +30,72 @@ theta.IS.agr <- with(sim.params, get_agr_IS(lambda, mu, thetaSI, thetaIS, ntypes
 #   colSums(sim.data.summ$mean.num.samples)
 # })
 # apply(variability, 1, sd)
+num.datasets <- 24
+ab.noise.levels <- c(0, 0.01, 0.05, 0.1, 0.5, 1)
 set.seed(2100)
-sim.data <- do.call(simulate_data, sim.params)
-mcmc_options <- list(niter="100000", sample_every=50, adapt_optimal=0.23, adapt_every=10,
-                  adapt_until="5000")
+sim.data.all.serotypes <- mclapply(1:num.datasets, function (i) {
+  sim.params$lambda <- c(rlnorm(ntypes, log(0.0003696), 2e-1), rlnorm(ntot-ntypes, log(0.0003696), 2e-1))*day.units
+  sim.params$mu <- rlnorm(ntot, log(0.016696), 2e-1)*day.units
+  sim.params$thetaSI <- rnorm(ntypes, 0.2505, 0.06)
+  sim.params$p0 <- rnorm(ntypes, 0.9, 0.3)
+  sim.data <- list(param=sim.params, data=do.call(simulate_data, sim.params)) # takes around 10.5 seconds per simulation
+  data.reps <- lapply(ab.noise.levels, function (ab.noise.level) {
+    sim.data$data$abdata <- sim.data$data$abdata + rnorm(length(sim.data$data$abdata), sd=ab.noise.level)
+    return (sim.data)
+  })
+  return (data.reps)
+}, mc.cores=4)
+set.seed(2000)
+sim.data.ind.var <- mclapply(ab.noise.levels, function (ab.noise.level) {
+  sim.params$lambda <- c(rlnorm(ntypes, log(0.0003696), 2e-1), rlnorm(ntot-ntypes, log(0.0003696), 2e-1))*day.units
+  sim.params$mu <- rlnorm(ntot, log(0.016696), 2e-1)*day.units
+  sim.params$thetaSI <- rep(rnorm(1, 0.2505, 0.06), ntypes)
+  sim.params$p0 <- rep(rnorm(1, 0.9, 0.3), ntypes)
+  sim.data <- list(param=sim.params, data=do.call(simulate_data, sim.params))
+  data.reps <- lapply(ab.noise.levels, function (ab.noise.level) {
+    sim.data$data$abdata <- sim.data$data$abdata + rnorm(length(sim.data$data$abdata), sd=ab.noise.level)
+    return (sim.data)
+  })
+  return (data.reps)
+}, mc.cores=4)
 
-mcmc_infer (sim.params, sim.params.sd, sim.data, mcmc_options, 
-            mcmc.program.dir="VaccInfer/VaccInfer", program.name="mcmc_infer",
-            run.program = FALSE)
 
+mcmc_options <- list(niter="500000", sample_every=100, adapt_optimal=0.23, adapt_every=10,
+                  adapt_until="5000", use_mean_ab=0)
 save.image(file="sim.data.RData")
+
+lapply(seq_along(sim.data.all.serotypes), function (i) {
+  lapply(seq_along(sim.data.all.serotypes[[i]]), function (j) {
+    mcmc_infer (sim.data.all.serotypes[[i]][[j]]$param, sim.params.sd, 
+                sim.data.all.serotypes[[i]][[j]]$data, mcmc_options, 
+                mcmc.program.dir="VaccInfer/VaccInfer", program.name="mcmc_infer",
+                run.program = FALSE, 
+                input.file=paste0("sim_data/all_sero_rep_", i, "_abnoise_", ab.noise.levels[j], ".txt"),
+                file.out=paste0("sim_data/output/all_sero_rep_", i, "_abnoise_", ab.noise.levels[j], ".txt"))
+    mcmc_options$use_mean_ab=1
+    mcmc_infer (sim.data.all.serotypes[[i]][[j]]$param, sim.params.sd, 
+                sim.data.all.serotypes[[i]][[j]]$data, mcmc_options, 
+                mcmc.program.dir="VaccInfer/VaccInfer", program.name="mcmc_infer",
+                run.program = FALSE, 
+                input.file=paste0("sim_data/meanab_rep_", i, "_abnoise_", ab.noise.levels[j], ".txt"),
+                file.out=paste0("sim_data/output/meanab_rep_", j, "_abnoise_", ab.noise.levels[j], ".txt"))
+  })
+})
+lapply(seq_along(sim.data.ind.var), function (i) {
+  lapply(seq_along(sim.data.ind.var[[i]]), function (j) {
+    mcmc_infer (sim.data.ind.var[[i]][[j]]$param, sim.params.sd, 
+                sim.data.ind.var[[i]][[j]]$data, mcmc_options, 
+                mcmc.program.dir="VaccInfer/VaccInfer", program.name="mcmc_infer",
+                run.program = FALSE, 
+                input.file=paste0("sim_data/ind_var_rep_", j, "_abnoise_", ab.noise.levels[i], ".txt"),
+                file.out=paste0("output/ind_var_rep_", j, "_abnoise_", ab.noise.levels[i], ".txt"))
+  })
+})
+
+# mcmc_infer (sim.params, sim.params.sd, sim.data, mcmc_options,
+#             mcmc.program.dir="VaccInfer/VaccInfer", program.name="mcmc_infer",
+#             run.program = FALSE)
+
 
 
 
