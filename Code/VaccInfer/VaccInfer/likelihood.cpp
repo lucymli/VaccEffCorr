230866653f81721e93fcf1f639ba95484b51bfa7
Lucy Li <mengqi.li09@imperial.ac.uk>
2017-05-08 14:42:01 -0400

    likelihood class

---
+++ Code/VaccInfer/VaccInfer/likelihood.cpp
@@ -0,0 +1,9 @@
+//
+//  likelihood.cpp
+//  VaccInfer
+//
+//  Created by Lucy Li on 5/8/17.
+//  Copyright © 2017 Lucy M Li, CCDD, HPSH. All rights reserved.
+//
+
+#include "likelihood.hpp"
---
+++ Code/VaccInfer/VaccInfer/likelihood.hpp
@@ -0,0 +1,14 @@
+//
+//  likelihood.hpp
+//  VaccInfer
+//
+//  Created by Lucy Li on 5/8/17.
+//  Copyright © 2017 Lucy M Li, CCDD, HPSH. All rights reserved.
+//
+
+#ifndef likelihood_hpp
+#define likelihood_hpp
+
+#include <stdio.h>
+
+#endif /* likelihood_hpp */
