ac48703462ada0cad57f610a0089d75bc4af0274
Lucy Li <mengqi.li09@imperial.ac.uk>
2017-05-17 10:01:32 -0400

    new data class

--- Code/VaccInfer/VaccInfer/data.cpp
+++ Code/VaccInfer/VaccInfer/data.cpp
@@ -9,7 +9,47 @@
 #include "data.hpp"
 
 
-
-Data::Data () {
-        
+double Data::get_carriage(int ind_i, int time_i) const {
+    return (carriage[time_i * n_ind + ind_i]);
 }
+
+double Data::get_metadata(int ind_i, int i) const {
+    return (metadata[i*n_ind + ind_i]);
+}
+
+double Data::get_predictor(int ind_i, int predict_i) const {
+    return (predictors[predict_i * n_ind + ind_i]);
+}
+
+double Data::get_inferred_risk(int ind_i, int i) const {
+    return (inferred_risk[i * n_ind + ind_i]);
+}
+
+double Data::sum_risk_by_ind(int ind_i) const {
+    double risk=0.0;
+    for (int i=0; i<n_vacc_types; i++) {
+        risk += inferred_risk[i*n_ind + ind_i];
+    }
+    return (risk);
+}
+
+double Data::sum_risk_by_type(int type_i) const {
+    double risk=0.0;
+    for (int i=0; i<n_ind; i++) {
+        risk += inferred_risk[type_i*n_ind+i];
+    }
+    return (risk);
+}
+
+double Data::mean_risk_by_ind(int ind_i) const {
+    double mean_risk = this->sum_risk_by_ind(ind_i);
+    mean_risk /= (double) n_vacc_types;
+    return (mean_risk);
+}
+
+double Data::mean_risk_by_type(int type_i) const {
+    double mean_risk = this->sum_risk_by_type(type_i);
+    mean_risk /= (double) n_ind;
+    return (mean_risk);
+}
+
--- Code/VaccInfer/VaccInfer/data.hpp
+++ Code/VaccInfer/VaccInfer/data.hpp
@@ -18,7 +18,17 @@ public:
     Data ();
     std::vector <double> carriage; // nrow=total number of individuals, ncol=number of swabs
     std::vector <double> metadata; // nrow=total number of individuals, ncol=number of covariates
-    std::vector <double> predictors; // nrow=total number of individuals, ncol=number of predictors of carraige (serotype-specific antibody levels)
+    std::vector <double> metadata_corr; //row=3, ncol=number of covariates; intercept, slope and R2
+    std::vector <double> predictors; // nrow=total number of individuals, ncol=number of predictors of carriage (serotype-specific antibody levels)
+    std::vector <double> inferred_risk; // nrow=total number of individuals, ncol=number of serotypes
+    double get_carriage(int, int) const;
+    double get_metadata(int, int) const;
+    double get_predictor(int, int) const;
+    double get_inferred_risk(int, int) const;
+    double sum_risk_by_ind(int) const;
+    double sum_risk_by_type(int) const;
+    double mean_risk_by_ind(int) const;
+    double mean_risk_by_type(int) const;
     int n_ind;
     int n_time;
     int n_types;
