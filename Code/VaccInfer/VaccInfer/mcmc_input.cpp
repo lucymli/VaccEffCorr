9dd2c812f6c08a76dfa45277c6ce5c6b29c1362a
Lucy Li <luli@hsph.harvard.edu>
2017-12-20 17:05:53 -0500

    option to read in MCMC options file

--- Code/VaccInfer/VaccInfer/mcmc_input.cpp
+++ Code/VaccInfer/VaccInfer/mcmc_input.cpp
@@ -20,3 +20,20 @@ MCMC::MCMC() {
     tot_blocks = 5;
     use_mean_ab = false;
 }
+
+MCMC::MCMC(std::string input_file) {
+    std::ifstream input;
+    input.open(input_file);
+    std::string param_name;
+    while (input) {
+        input >> param_name;
+        if (param_name=="iter") input >> iter;
+        if (param_name=="niter") input >> niter;
+        if (param_name=="sample_every") input >> sample_every;
+        if (param_name=="adapt_optimal") input >> adapt_optimal;
+        if (param_name=="adapt_every") input >> adapt_every;
+        if (param_name=="adapt_until") input >> adapt_until;
+        if (param_name=="tot_blocks") input >> tot_blocks;
+        if (param_name=="use_mean_ab") input >> use_mean_ab;
+    }
+}
