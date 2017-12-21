9f56460dc359f289c6ddd52c12443050edfc70da
Lucy Li <luli@hsph.harvard.edu>
2017-12-21 15:52:00 -0500

    new proposal function

--- Code/VaccInfer/VaccInfer/param_input.cpp
+++ Code/VaccInfer/VaccInfer/param_input.cpp
@@ -40,6 +40,7 @@ Param::Param (std::string param_file_name) {
         params_names.push_back(temp_str);
         input >> temp_double;
         params.push_back(temp_double);
+        params_sd.push_back(temp_double*0.5);
         input >> temp_str;
         params_trans.push_back(temp_str);
         input >> temp_double;
@@ -62,6 +63,7 @@ Param::Param (std::string param_file_name) {
     lprior = 0.0;
     new_lprior = 0.0;
     block_ptr = 0;
+    param_index = 0;
     accepted.resize(n_blocks, 0);
     rejected.resize(n_blocks, 0);
 }
@@ -69,7 +71,7 @@ Param::Param (std::string param_file_name) {
 void Param::print_params_to_screen() const {
     std::cout << "Parameter table" << std::endl;
     std::cout << "Name\tValue\tTransformation\tMin\tMax\tPrior\tPriorParam1\tPriorParam2" <<std::endl;
-    for (int i=0; i<params.size(); ++i) {
+    for (int i=0; i<params.size(); i++) {
         std::cout << params_names[i] << "\t";
         std::cout << params[i] << "\t";
         std::cout << params_trans[i] << "\t";
@@ -99,7 +101,7 @@ double Param::calc_lprior(bool print = false) {
     double dens = 0.0;
     double param_value;
     double curr_dens = 0.0;
-    for (int i=0; i<params.size(); ++i) {
+    for (int i=0; i<params.size(); i++) {
         param_value = transform(params_trans[i], params[i]);
         curr_dens = get_density(param_value, params_prior[i], params_prior1[i], params_prior2[i], true);
         dens += curr_dens;
@@ -107,4 +109,12 @@ double Param::calc_lprior(bool print = false) {
     }
     if (dens < SMALLEST_NUMBER) dens = SMALLEST_NUMBER;
     return (dens);
+}
+
+void Param::propose() {
+    int ntries = 50;
+    double tempparam = params[param_index];
+    double transformed_val = transform(params_trans[param_index], tempparam);
+    double rnum = rnorm(transformed_val, params_sd[param_index], params_min[param_index], params_max[param_index], ntries);
+    params[param_index] = transform(params_trans[param_index], rnum, true);
 }
