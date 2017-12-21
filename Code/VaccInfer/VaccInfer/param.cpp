2bcfa4bfaecb3eee7cd229d3602a59dae3626615
Lucy Li <luli@hsph.harvard.edu>
2017-12-21 16:37:19 -0500

    remove block parameters

--- Code/VaccInfer/VaccInfer/param.cpp
+++ Code/VaccInfer/VaccInfer/param.cpp
@@ -34,21 +34,17 @@ void Param::next () {
 void Param::accept_reject () {
     bool accept_this = false;
     double ratio = (new_llik+new_lprior)-(llik+lprior);
-//    std::cout << "new_llik: " << new_llik;
-//    std::cout << " new_lprior: " << new_lprior;
-//    std::cout << " llik: " << llik;
-//    std::cout << " lprior: " << lprior << std::endl;
     double z = log(runif());
     if (z < ratio) accept_this = true;
     if (accept_this) {
         llik = new_llik;
         lprior = new_lprior;
-        accepted[block_ptr] += 1;
+        accepted[param_ptr] += 1;
     }
     else {
         params.swap(tempparam);
         inferred_risk.swap(inferred_risk_temp);
-        rejected[block_ptr] += 1;
+        rejected[param_ptr] += 1;
     }
     new_llik = SMALLEST_NUMBER;
     new_lprior = SMALLEST_NUMBER;
@@ -65,48 +61,5 @@ void Param::print_to_file (int iter) {
     output_file.close();
 }
 
-
-double Param::get_param(int block_i, int i) const {
-    return (params[block_starts[block_i]+i]);
-}
-
-double Param::get_inferred_risk(int ind_i, int i) const {
-    double risk = inferred_risk[i*n_ind+ind_i];
-    return (risk);
-}
-
-void Param::set_inferred_risk(double x, int ind_i, int i) {
-    if (x > 1.0) x = 1.0;
-    if (x < 0.0) x = 0.0;
-    inferred_risk[i * n_ind + ind_i] = x;
-}
-
-double Param::sum_risk_by_ind(int ind_i) const {
-    double risk=0.0;
-    for (int i=0; i<n_vtypes; i++) {
-        risk += inferred_risk[i*n_ind + ind_i];
-    }
-    return (risk);
-}
-
-double Param::sum_risk_by_type(int type_i) const {
-    double risk=0.0;
-    for (int i=0; i<n_ind; i++) {
-        risk += inferred_risk[type_i*n_ind+i];
-    }
-    return (risk);
-}
-
-double Param::mean_risk_by_ind(int ind_i) const {
-    double mean_risk = this->sum_risk_by_ind(ind_i);
-    mean_risk /= (double) n_vtypes;
-    return (mean_risk);
-}
-
-double Param::mean_risk_by_type(int type_i) const {
-    double mean_risk = this->sum_risk_by_type(type_i);
-    mean_risk /= (double) n_ind;
-    return (mean_risk);
-}
 
 
