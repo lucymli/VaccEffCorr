c22ec140b5a338e301c757923db75944f406bbb2
Lucy Li <luli@hsph.harvard.edu>
2017-12-25 14:45:46 -0500

    data --> sim_data

--- Code/VaccInfer/VaccInfer/simulation.cpp
+++ Code/VaccInfer/VaccInfer/simulation.cpp
@@ -8,17 +8,15 @@
 
 #include "simulation.hpp"
 
-void Simulation::Simulation(Param parameters, Data sim_data) {
+Simulation::Simulation(Param parameters, Data sim_data) {
     int nthread = 4;//OMP_NUM_THREADS;
     arma::mat base (parameters.n_tot+1, parameters.n_tot+1);
-    fill_rates(parameters, base);
-    double time_diff;
-    int previous, now;
-    #pragma omp parallel for schedule(static, 1)
+    if (false) {fill_rates(parameters, base);
+    //#pragma omp parallel for schedule(static, 1)
     for (int tn=0; tn<nthread; tn++) {
         arma::mat exp_mat = arma::expmat(base*sim_data.times[0]);
         std::vector <double> cumulative (1, exp_mat(0, 0));
-        for (int i=1; i<=n_tot; i++) cumulative.push_back(cumulative[i-1]+exp_mat(0, i));
+        for (int i=1; i<=sim_data.n_tot; i++) cumulative.push_back(cumulative[i-1]+exp_mat(0, i));
         for (int ind_i=tn; ind_i<sim_data.n_ind; ind_i+=nthread) {
             double rnum = runif();
             double x = cumulative[0];
@@ -31,15 +29,15 @@ void Simulation::Simulation(Param parameters, Data sim_data) {
             sim_data.set_carriage(ind_i, 0, (double) event-1);
         }
     }
-    for (int time_t=1; time_t<data.n_time; time_t++) {
-#pragma omp parallel for schedule(static, 1)
+    for (int time_t=1; time_t<sim_data.n_time; time_t++) {
+//#pragma omp parallel for schedule(static, 1)
         for (int tn=0; tn<nthread; tn++) {
             double time_interval = sim_data.times[time_t] - sim_data.times[time_t-1];
             arma::mat exp_mat = arma::expmat(base*time_interval);
             for (int ind_i=tn; ind_i<sim_data.n_ind; ind_i+=nthread) {
                 int prev = sim_data.get_carriage(ind_i, time_t-1);
                 std::vector <double> cumulative (1, exp_mat(prev, 0));
-                for (int i=1; i<=n_tot; i++) cumulative.push_back(cumulative[i-1]+exp_mat(prev, i));
+                for (int i=1; i<=sim_data.n_tot; i++) cumulative.push_back(cumulative[i-1]+exp_mat(prev, i));
                 double rnum = runif();
                 double x = cumulative[0];
                 int event = 0;
@@ -51,5 +49,5 @@ void Simulation::Simulation(Param parameters, Data sim_data) {
                 sim_data.set_carriage(ind_i, 0, (double) event-1);
             }
         }
-    }
+    }}
 }
