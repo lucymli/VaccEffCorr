20f9b4d9e3843e9a4f44d777fc8e48af3d3ce97a
Lucy Li <luli@hsph.harvard.edu>
2018-01-01 18:26:08 -0500

    merge mcmc and mcmc_input files

--- Code/VaccInfer/VaccInfer/mcmc.cpp
+++ Code/VaccInfer/VaccInfer/mcmc.cpp
@@ -93,3 +93,33 @@ void MCMC::run_mcmc(Param parameters, Data data) {
         parameters.next();
     }
 }
+
+
+MCMC::MCMC() {
+    iter = 0;
+    niter = 100000;
+    sample_every = 100;
+    adapt_optimal = 0.23;
+    adapt_every = 10;
+    adapt_until = 100;
+    tot_blocks = 5;
+    use_mean_ab = false;
+}
+
+MCMC::MCMC(std::string input_file) {
+    std::ifstream input;
+    input.open(input_file);
+    std::string param_name;
+    while (input) {
+        input >> param_name;
+        if (param_name=="iter") input >> iter;
+        if (param_name=="niter") input >> niter;
+        if (param_name=="sample_every") input >> sample_every;
+        if (param_name=="adapt_optimal") input >> adapt_optimal;
+        if (param_name=="adapt_every") input >> adapt_every;
+        if (param_name=="adapt_until") input >> adapt_until;
+        if (param_name=="tot_blocks") input >> tot_blocks;
+        if (param_name=="use_mean_ab") input >> use_mean_ab;
+    }
+}
+
