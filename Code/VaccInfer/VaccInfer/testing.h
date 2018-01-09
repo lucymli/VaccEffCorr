fd813490677b9c904e379f65afe4ee2dbdabdb1f
Lucy Li <luli@hsph.harvard.edu>
2018-01-09 10:39:15 -0500

    testing header file

---
+++ Code/VaccInfer/VaccInfer/testing.h
@@ -0,0 +1,14 @@
+//
+//  testing.h
+//  VaccInfer
+//
+//  Created by Lucy Li on 1/9/18.
+//  Copyright © 2018 Lucy M Li, CCDD, HPSH. All rights reserved.
+//
+
+#ifndef testing_h
+#define testing_h
+
+void horizontal_rule ();
+
+#endif /* testing_h */
