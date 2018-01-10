e9e5f9e6dbb85ec69983eff9b5ad2dca22fde997
Lucy Li <luli@hsph.harvard.edu>
2018-01-10 09:52:35 -0500

    read in from file

--- Code/VaccInfer/VaccInfer/data.cpp
+++ Code/VaccInfer/VaccInfer/data.cpp
@@ -9,6 +9,58 @@
 #include <iostream>
 #include "data.hpp"
 
+Data::Data () {
+    n_ind = 0;
+    n_tot = 0;
+    n_time = 0;
+    n_vtypes = 0;
+    n_predictors = 0;
+}
+
+Data::Data (std::string filename) {
+    std::ifstream input;
+    input.open(filename);
+    std::string name;
+    double number;
+    int integer;
+    while (input.good()) {
+        input >> name;
+        if (name == "n_ind") input >> n_ind;
+        if (name == "n_tot") input >> n_tot;
+        if (name == "n_time") input >> n_time;
+        if (name == "n_predictors") input >> n_predictors;
+        n_vtypes = n_predictors;
+        if (name == "times") {
+            for (int i=0; i<n_time; i++) {
+                input >> number;
+                times.push_back(number);
+            }
+        }
+        if (name == "carriage") {
+            carriage.resize(n_ind*n_time);
+            for (int i=0; i<n_ind*n_time; i++) {
+                input >> number;
+                set_carriage(i%n_ind, i/n_ind, number);
+            }
+        }
+        if (name == "predictors") {
+            predictors.resize(n_ind*n_predictors);
+            for (int i=0; i<n_ind*n_predictors; i++) {
+                input >> number;
+                set_predictor(i%n_ind, i/n_ind, number);
+            }
+        }
+        if (name == "predictor_map") {
+            predictors.resize(n_ind*n_predictors);
+            for (int i=0; i<n_ind*n_predictors; i++) {
+                input >> integer;
+                set_predictor_index(i%n_ind, i/n_ind, number);
+            }
+        }
+    }
+    input.close();
+}
+
 Data::Data (int num_ind, int num_types, int num_times, std::vector <double> time_points, int num_predictors) {
     // Generates an empty Data class
     for (int i=0; i<(num_ind*num_times); i++) carriage.push_back(0);
